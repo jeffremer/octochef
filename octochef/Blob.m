29a4ba583ee020e9ee07d0796270134e8cc5a02d
smithclay <smithclay@gmail.com>
2011-07-16 16:43:29 -0700

    added more models

---
+++ octochef/Blob.m
@@ -0,0 +1,14 @@
+//
+//  Blob.m
+//  octochef
+//
+//  Created by Clay Smith on 7/16/11.
+//  Copyright 2011 Widgetbox, Inc. All rights reserved.
+//
+
+#import "Blob.h"
+
+
+@implementation Blob
+
+@end
