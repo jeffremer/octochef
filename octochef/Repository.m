de5cb0ff70acd1475c0322a75aa6c157f293ba4b
smithclay <smithclay@gmail.com>
2011-07-16 14:10:12 -0700

    added Repository model, removed TouchJSON

---
+++ octochef/Repository.m
@@ -0,0 +1,14 @@
+//
+//  Repository.m
+//  octochef
+//
+//  Created by Clay Smith on 7/16/11.
+//  Copyright 2011 Widgetbox, Inc. All rights reserved.
+//
+
+#import "Repository.h"
+
+
+@implementation Repository
+
+@end
